1ac9338ea2f8b0ca3bbd8da2ad7ab66e1b9e691b
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2024-11-27 09:33:21 +0000

    Fix linting error for unit tests

--- src/cleanup.test.ts
+++ src/cleanup.test.ts
@@ -1,6 +1,6 @@
 import { getRemotePostId } from "./cleanup.js";
 
-test("Test original broken URL format", () => {
+test("Original broken URL format", () => {
     const input = "https://www.reddit.com//r/BeAmazed/comments/1gwd56x/bishnu_shrestha_fought_off_40_armed_robbers_on_a/";
     const expected = "t3_1gwd56x";
 
@@ -9,7 +9,7 @@ test("Test original broken URL format", () => {
     expect(actual).toEqual(expected);
 });
 
-test("Test correct URL format", () => {
+test("Correct URL format", () => {
     const input = "https://www.reddit.com/r/GenshinImpact/comments/1h0nnwv/the_main_sub_really_deleted_this_post/";
     const expected = "t3_1h0nnwv";
 
